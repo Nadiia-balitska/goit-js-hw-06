4f321908520f8986bf30fcdb1c4756378c7ff096
Nadiia Balitska <161743763+Nadiia-balitska@users.noreply.github.com>
2024-04-17 15:06:32 +0300

    fix task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,15 +1,14 @@
-let quantity = null;
-let pricePerDroid = null;
-let totalPrice = quantity * pricePerDroid;
-const makeTransaction = message;
-const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-quantity = 3;
-pricePerDroid = 1000;
-console.log(message);
- 
-
-
+function makeTransaction(quantity, pricePerDroid) {
+    let totalPrice = quantity * pricePerDroid;
+    const makeTransaction = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    return makeTransaction;
+}
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+ 
+
+
+
